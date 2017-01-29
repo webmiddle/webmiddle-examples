b1a72d9e9468176aeedc068cc6b76b32fe7f1af9
Manuel Dell'Elce <mh025r0x@gmail.com>
2017-01-29 12:59:54 +0100

    Delete

--- src/example/news-search/utils.js
+++
@@ -1,7 +0,0 @@
-// e.g. "Jan 4, 2016"
-export function getFormattedDate(date) {
-  const month = date.toLocaleString('en-us', { month: 'short' });
-  const day = date.getDay() + 1;
-  const year = date.getFullYear();
-  return `${month} ${day}, ${year}`;
-}
--- src/example/sites/nytimes.com/config.js
+++
@@ -1,3 +0,0 @@
-export default {
-  resultsPerPage: 10,
-};
